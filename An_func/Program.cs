ed927b151e5257488481afa4fd978abb84f7447f
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-04-06 01:42:43 +0300

    initiall commit

--- An_func/Program.cs
+++ An_func/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+
+namespace LambdaExpressionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, string, bool> containsWord = (text, word) => text.Contains(word);
+
+            Console.WriteLine("Enter a text:");
+            string text = Console.ReadLine();
+
+            Console.WriteLine("Enter a word to check:");
+            string word = Console.ReadLine();
+
+            bool result = containsWord(text, word);
+            Console.WriteLine($"The text {(result ? "contains" : "does not contain")} the word '{word}'.");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using LambdaExpressionExample;
+
+namespace UniqueNegativeNumbers.Test
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void UniqueNegativeNumbers_ReturnsCorrectResult()
+        {
+            int[] numbers = { -1, 2, -3, 4, -5, 2, -7, 4, -9, 10 };
+
+            var uniqueNegativeNumbers = numbers.Where(x => x < 0).Distinct().ToArray();
+
+            Assert.AreEqual(5, uniqueNegativeNumbers.Length);
+            Assert.IsTrue(uniqueNegativeNumbers.Contains(-1));
+            Assert.IsTrue(uniqueNegativeNumbers.Contains(-3));
+            Assert.IsTrue(uniqueNegativeNumbers.Contains(-5));
+            Assert.IsTrue(uniqueNegativeNumbers.Contains(-7));
+            Assert.IsTrue(uniqueNegativeNumbers.Contains(-9));
+        }
+    }
+}
