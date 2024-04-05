a10f85e39789d9f8b817de80b99545d07acbc677
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-04-06 01:30:32 +0300

    initiall commit

--- An_func/Program.cs
+++ An_func/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { -1, 2, -3, 4, -5, 2, -7, 4, -9, 10 };
+
+        var uniqueNegativeNumbers = numbers.Where(x => x < 0).Distinct();
+
+        foreach (var number in uniqueNegativeNumbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
